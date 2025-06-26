375493ad0aa67edfdc26ce627d2e7eb8ce5f434f
Google. Nn <9198951281shubham@gmail.com>
2025-06-26 07:13:57 +0530

    Update script.js
    Animated explanation

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function showMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+  const menu = document.getElementById("menu");
+  if (menu.style.display === "none" || menu.style.display === "") {
+    menu.style.display = "block";
+    menu.classList.add("animated");
+  } else {
+    menu.style.display = "none";
+  }
 }

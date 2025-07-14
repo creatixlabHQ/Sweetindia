dbdd081a17e2b18f824f442c73653753adda0e2c
Google. Nn <9198951281shubham@gmail.com>
2025-07-14 16:22:38 +0530

    Update script.js
    U

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function showMenu() {
-  const menu = document.getElementById("menu");
 
-  if (menu.style.display === "none" || menu.style.display === "") {
-    menu.style.display = "block";
-    menu.classList.remove("hide");
-    menu.classList.add("show");
-  } else {
-    menu.classList.remove("show");
-    menu.classList.add("hide");
-    setTimeout(() => {
-      menu.style.display = "none";
-    }, 800); // Match hide animation duration
-  }
-}
